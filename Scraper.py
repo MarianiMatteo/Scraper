acde682c2b71d60070fa84ad963c9b846d32b480
matz-9 <matteom0797@me.com>
2023-01-17 14:53:21 +0100

    scrappy fix

--- Scraper.py
+++ Scraper.py
@@ -2,7 +2,7 @@ from apify_client import ApifyClient
 from Utility import *
 client = ApifyClient("apify_api_sK0VFtrbVbixtUIQasaJj46H6MbWFL2jitER")
 
-temp_url = "https://www.instagram.com/p/CeRZ7dUtvnb/"
+#temp_url = "https://www.instagram.com/p/CeRZ7dUtvnb/"
 
 links = scrappy_automator("post_urls.txt", client, 200)
 print(links)
