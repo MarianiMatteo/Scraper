6816d6a36c910929525b92acb11ae06366ae5ed0
MatteoMariani <matteo.mariani.1@studenti.unipd.it>
2023-02-10 14:16:02 +0100

    wordcloud

--- Scraper.py
+++ Scraper.py
@@ -3,10 +3,9 @@ from Utility import *
 import time
 
 
-#client = ApifyClient("apify_api_CV1KyT7LXglO43g6YtQHvl9t2cdhmD3kRm9h")
-# token = uEq8jDPC08AzZF17URS6cRN3hMGsxHmLgIgP5NlFZJMg4WGrl14dwGVAxvahIFay5nxEQyVaLbVmZL1zmP7DJZpdZmjyYJJNWBQGfdQxSJPWA5Gk7R3A8cgs70O3Ikn0
+# client = ApifyClient("apify_api_CV1KyT7LXglO43g6YtQHvl9t2cdhmD3kRm9h")
 # Fgl8ujy0qgiL7aiGsHGPcbmZi1fk2Tu8T9b8K4OFXmPdvbKlp599jzTt73RbqnDX1GL2cYfCLXEXSSzTvGt2yKfWXhRolB6oDbva0qwLXg7jNK6XNPiosVIA5f4eHaaF
-api_key = "uEq8jDPC08AzZF17URS6cRN3hMGsxHmLgIgP5NlFZJMg4WGrl14dwGVAxvahIFay5nxEQyVaLbVmZL1zmP7DJZpdZmjyYJJNWBQGfdQxSJPWA5Gk7R3A8cgs70O3Ikn0"
+api_key = "Fgl8ujy0qgiL7aiGsHGPcbmZi1fk2Tu8T9b8K4OFXmPdvbKlp599jzTt73RbqnDX1GL2cYfCLXEXSSzTvGt2yKfWXhRolB6oDbva0qwLXg7jNK6XNPiosVIA5f4eHaaF"
 client = ApifyClient("apify_api_CV1KyT7LXglO43g6YtQHvl9t2cdhmD3kRm9h")
 #temp_url = "https://www.instagram.com/p/CeRZ7dUtvnb/"
 
