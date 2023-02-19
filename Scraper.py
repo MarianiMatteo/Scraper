a9e1db8bb11b12c7665b96f079c5fd73a1adb41e
fra pen <44951845+francescopenna@users.noreply.github.com>
2023-02-19 17:34:34 +0100

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -6,8 +6,7 @@ api_key = "INSERT HERE YOUR ZEMBRA KEY"
 client = ApifyClient("INSERT HERE YOUR APIFY KEY")
 #temp_url = "https://www.instagram.com/p/CeRZ7dUtvnb/"
 
-#prepare_for_scraping()
+prepare_for_scraping()
 links = scrappy_automator("post_urls.txt", api_key, client)
-#print(links)
 
 detect_fake_accounts()
