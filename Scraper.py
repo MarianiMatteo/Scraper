18663d2b08ad86dc3309adf3d4258d7907463db3
fra pen <44951845+francescopenna@users.noreply.github.com>
2023-02-19 17:33:03 +0100

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -2,11 +2,8 @@ from apify_client import ApifyClient
 from Utility import *
 import time
 
-
-# client = ApifyClient("apify_api_CV1KyT7LXglO43g6YtQHvl9t2cdhmD3kRm9h")
-# Fgl8ujy0qgiL7aiGsHGPcbmZi1fk2Tu8T9b8K4OFXmPdvbKlp599jzTt73RbqnDX1GL2cYfCLXEXSSzTvGt2yKfWXhRolB6oDbva0qwLXg7jNK6XNPiosVIA5f4eHaaF
-api_key = "uEq8jDPC08AzZF17URS6cRN3hMGsxHmLgIgP5NlFZJMg4WGrl14dwGVAxvahIFay5nxEQyVaLbVmZL1zmP7DJZpdZmjyYJJNWBQGfdQxSJPWA5Gk7R3A8cgs70O3Ikn0"
-client = ApifyClient("apify_api_CV1KyT7LXglO43g6YtQHvl9t2cdhmD3kRm9h")
+api_key = "INSERT HERE YOUR ZEMBRA KEY"
+client = ApifyClient("INSERT HERE YOUR APIFY KEY")
 #temp_url = "https://www.instagram.com/p/CeRZ7dUtvnb/"
 
 #prepare_for_scraping()
